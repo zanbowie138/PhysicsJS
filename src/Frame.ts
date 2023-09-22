7dbb0e71b4349f58c3e9bf5455bc8dc4706b4f1e
Alexander Bui <72674604+zanbowie138@users.noreply.github.com>
2023-09-22 20:18:05 +0000

    rotating cube :)

--- src/Frame.ts
+++ src/Frame.ts
@@ -1,13 +1,35 @@
-import {WebGL} from './WebGL'
+import * as THREE from 'three';
+import {WebGL} from './WebGL';
 
 export class Frame {
     private gl: WebGL;
 
-    constructor(container: HTMLElement) {
+    constructor() {
         // Initialize WebGL class
         this.gl = new WebGL();
 
+        this.init();
+    }
+
+    public attach(container: HTMLElement) {
         // Add canvas to html element
         this.gl.setup(container);
+    }
+
+    public init() {
+        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        const cube = new THREE.Mesh( geometry, material );
+        this.gl.scene.add( cube );
+
+        this.gl.camera.position.z = 5;
+
+        const anim = () => {
+            this.gl.render()
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+        }
+
+        this.gl.requestAnimationFrame(anim)
     }
 }
--- src/entry.ts
+++ src/entry.ts
@@ -1,3 +1,13 @@
 import {Frame} from './Frame'
 
-const frame = new Frame(document.querySelector<HTMLDivElement>('.webgl-canvas__container'));
+const container = document.querySelector<HTMLDivElement>('.webgl-canvas__container');
+
+if (container != null) {
+    let m_canvas = new Frame();
+    m_canvas.attach(container);
+    // window.addEventListener('beforeunload', () => {
+    //     m_canvas.dispose();
+    //   })
+} else {
+    console.error("WebGL canvas container not found!");
+}
